67fcaf3c3bb080e2ea1f126bf498584f5959a46f
Pallabi Biswas <pallabibiswas4002@gmail.com>
2025-06-25 20:44:53 +0530

    Updated footer section in index.html

---
+++ operator.py
@@ -0,0 +1,35 @@
+name = "Tony Stark"
+print("y S" in name)
+
+# Arithmetic operator
+print(7-2)
+print(7*2)
+print(7/2)  # it print decimal value
+print(7//2) # it print only the integer part
+
+print(7 % 2) # reminder
+print(7 ** 2) # to the power
+
+# i = 5
+# i += 2 | i = i+2
+# i *= 2 | i = i*2
+# i /= 2 | i = i/2
+
+result = 2 + 3 * 5  #ans will be 17 or 25
+print(result) # = 17 so * has higher priority then +
+
+r = (2 + 3) * 5  #ans will be 17 or 25
+print(r) # () has more priority
+
+
+# Comparision operator
+
+print(3!=2)
+
+# Logical Operator
+
+print(2>3 or 2>1)  # if any one condition is true then true
+print(2>3 and 2>1) # if any one condition is false then false
+
+print(not 3>2)
+
